d21bc8aa091bfeab1aaa19210ca74b1f0e15c63f
libertyernie <maybeway36@gmail.com>
2016-04-01 19:29:37 -0500

    Add missing file

---
+++ LoopingAudioConverter/Brawl/RSTMImporter.cs
@@ -0,0 +1,43 @@
+﻿using BrawlLib.Wii.Audio;
+using RSTMLib.WAV;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace LoopingAudioConverter.Brawl {
+	public class RSTMImporter : IAudioImporter {
+		public bool SupportsExtension(string extension) {
+			if (extension.StartsWith(".")) extension = extension.Substring(1);
+			return extension.Equals("brstm", StringComparison.InvariantCultureIgnoreCase)
+				|| extension.Equals("bcstm", StringComparison.InvariantCultureIgnoreCase)
+				|| extension.Equals("bfstm", StringComparison.InvariantCultureIgnoreCase);
+		}
+
+		public PCM16Audio ReadFile(string filename) {
+			if (filename.Contains('"')) {
+				throw new AudioImporterException("File paths with double quote marks (\") are not supported");
+			}
+
+			byte[] data = File.ReadAllBytes(filename);
+			if (data.Length == 0) {
+				throw new AudioImporterException("Empty input file");
+			}
+			try {
+				if (data[0] == 'C') {
+					data = CSTMConverter.ToRSTM(data);
+				} else if (data[0] == 'F') {
+					data = FSTMConverter.ToRSTM(data);
+				}
+			} catch (Exception e) {
+				throw new AudioImporterException("Could not convert from B" + (char)data[0] + "STM: " + e.Message);
+			}
+
+			return PCM16Factory.FromAudioStream(RSTMConverter.CreateStreams(data)[0]);
+		}
+
+		public string GetImporterName() {
+			return "BrawlLib/RSTMLib";
+		}
+	}
+}
