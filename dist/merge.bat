d1e6b00b2657120003cc2890e7724e0b3b3ea6d7
libertyernie <maybeway36@gmail.com>
2016-11-04 14:49:28 -0500

    1.3.3: RSTM+PCM16 support

--- dist/merge.bat
+++ dist/merge.bat
@@ -7,5 +7,5 @@ XCOPY /E /I ..\LoopingAudioConverter\tools_linux-i686 linux32\tools
 XCOPY ..\LoopingAudioConverter\About.html linux32
 SET FROM=..\stm-encode\bin\Release
 "C:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe" /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /out:stm-encode.exe %FROM%\stm-encode.exe %FROM%\RSTMLib.dll
-DEL windows\LoopingAudioConverter.exe.pdb stm-encode.pdb
+DEL windows\LoopingAudioConverter.pdb stm-encode.pdb
 @PAUSE
