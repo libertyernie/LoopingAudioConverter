962d0e9aae6c8fb69eb5b2c91a7339eab155724b
libertyernie <maybeway36@gmail.com>
2018-01-04 17:30:17 -0600

    Update merge.bat

--- dist/merge.bat
+++ dist/merge.bat
@@ -1,8 +1,11 @@
 SET FROM=..\LoopingAudioConverter\bin\Release
-"C:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe" /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5" /out:windows\LoopingAudioConverter.exe %FROM%\LoopingAudioConverter.exe %FROM%\VGAudio.dll %FROM%\LoopSelectionDialogLib.dll %FROM%\VorbisCommentSharp.dll
+"C:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe" /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5" /out:windows\LoopingAudioConverter.exe %FROM%\LoopingAudioConverter.exe %FROM%\VGAudio.dll %FROM%\BrawlLibAudio.dll %FROM%\VorbisCommentSharp.dll
 XCOPY /E /I ..\LoopingAudioConverter\tools_win32 windows\tools
 XCOPY ..\LoopingAudioConverter\About.html windows
 XCOPY windows\LoopingAudioConverter.exe linux32
 XCOPY /E /I ..\LoopingAudioConverter\tools_linux-i686 linux32\tools
 XCOPY ..\LoopingAudioConverter\About.html linux32
+XCOPY windows\LoopingAudioConverter.exe linux64
+XCOPY /E /I ..\LoopingAudioConverter\tools_linux-amd64 linux64\tools
+XCOPY ..\LoopingAudioConverter\About.html linux64
 @PAUSE
