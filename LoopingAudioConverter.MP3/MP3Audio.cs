4daf0b7715f2df45e1ff774105a631a47fa88fb0
libertyernie <maybeway36@gmail.com>
2022-12-14 10:46:07 -0600

    Remove MP3Sharp

--- LoopingAudioConverter.MP3/MP3Audio.cs
+++ LoopingAudioConverter.MP3/MP3Audio.cs
@@ -1,32 +1,9 @@
-﻿using LoopingAudioConverter.PCM;
-using MP3Sharp;
-using System;
-using System.IO;
-using System.Runtime.InteropServices;
-
-namespace LoopingAudioConverter.MP3 {
+﻿namespace LoopingAudioConverter.MP3 {
 	public sealed class MP3Audio {
 		public readonly byte[] Data;
 
 		public MP3Audio(byte[] data) {
 			Data = data;
-		}
-
-		public unsafe PCM16Audio Decode() {
-			using (var output = new MemoryStream())
-			using (var input = new MemoryStream(Data, false))
-			using (var mp3 = new MP3Stream(input)) {
-				mp3.CopyTo(output);
-				byte[] array = output.ToArray();
-
-				short[] samples = new short[array.Length / sizeof(short)];
-				fixed (byte* ptr = array) {
-					short* ptr16 = (short*)ptr;
-					Marshal.Copy((IntPtr)ptr16, samples, 0, samples.Length);
-				}
-
-				return new PCM16Audio(mp3.ChannelCount, mp3.Frequency, samples);
-			}
 		}
 	}
 }
