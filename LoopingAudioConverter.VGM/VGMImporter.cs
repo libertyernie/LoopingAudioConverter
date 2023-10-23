202a71cdc29f6db3fec98a127a439dcb5181f730
libertyernie <maybeway36@gmail.com>
2023-10-22 21:25:30 -0500

    Do not use ffmpeg to play vgm files (something about how it renders them throws off the original loop points in the file)

--- LoopingAudioConverter.VGM/VGMImporter.cs
+++ LoopingAudioConverter.VGM/VGMImporter.cs
@@ -13,16 +13,7 @@ namespace LoopingAudioConverter.VGM {
 	/// A class to use either FFmpeg or VGMPlay to render VGM/VGZ files to WAV format.
 	/// </summary>
 	public class VGMImporter : IAudioImporter {
-		private readonly FFmpegEngine Engine;
 		private readonly string VGMPlayPath;
-
-		/// <summary>
-		/// Initializes the VGM importer.
-		/// </summary>
-		/// <param name="engine">The FFmpeg handler</param>
-		public VGMImporter(FFmpegEngine engine) {
-			Engine = engine;
-		}
 
 		/// <summary>
 		/// Initializes the VGM importer.
@@ -82,59 +73,50 @@ namespace LoopingAudioConverter.VGM {
 					loopSamples = br.ReadInt32();
 				}
 
-				PCM16Audio data;
-				if (VGMPlayPath != null) {
-					string tmpDir = Path.Combine(Path.GetTempPath(), "LoopingAudioConverter-" + Guid.NewGuid());
-					Directory.CreateDirectory(tmpDir);
+				string tmpDir = Path.Combine(Path.GetTempPath(), "LoopingAudioConverter-" + Guid.NewGuid());
+				Directory.CreateDirectory(tmpDir);
 
-					string inFile = Path.Combine(tmpDir, "audio" + Path.GetExtension(filename));
-					File.Copy(filename, inFile);
-					using (var sw = new StreamWriter(new FileStream(Path.Combine(tmpDir, "VGMPlay.ini"), FileMode.Create, FileAccess.Write))) {
-						sw.WriteLine("[General]");
-						sw.WriteLine("SampleRate = " + hints.RenderingSampleRate);
-						sw.WriteLine("FadeTime = 0");
-						sw.WriteLine("LogSound = 1");
-						sw.WriteLine("MaxLoops = 0x01");
-					}
+				string inFile = Path.Combine(tmpDir, "audio" + Path.GetExtension(filename));
+				File.Copy(filename, inFile);
+				using (var sw = new StreamWriter(new FileStream(Path.Combine(tmpDir, "VGMPlay.ini"), FileMode.Create, FileAccess.Write))) {
+					sw.WriteLine("[General]");
+					sw.WriteLine("SampleRate = " + hints.RenderingSampleRate);
+					sw.WriteLine("FadeTime = 0");
+					sw.WriteLine("LogSound = 1");
+					sw.WriteLine("MaxLoops = 0x01");
+				}
 
-					ProcessStartInfo psi = new ProcessStartInfo {
-						FileName = Path.GetFullPath(VGMPlayPath),
-						WorkingDirectory = tmpDir,
-						UseShellExecute = false,
-						CreateNoWindow = true,
-						RedirectStandardOutput = true,
-						Arguments = inFile
-					};
+				ProcessStartInfo psi = new ProcessStartInfo {
+					FileName = Path.GetFullPath(VGMPlayPath),
+					WorkingDirectory = tmpDir,
+					UseShellExecute = false,
+					CreateNoWindow = true,
+					RedirectStandardOutput = true,
+					Arguments = inFile
+				};
 
-					var process = Process.Start(psi);
+				var process = Process.Start(psi);
 
-					string line;
-					double pr = 0.0;
-					using (var sr = process.StandardOutput) {
-						while ((line = await sr.ReadLineAsync()) != null) {
-							if (line.StartsWith("Playing ")) {
-								int stop = line.IndexOf('%');
-								if (stop > 8 && double.TryParse(line.Substring(8, stop - 8), out double percentage) && percentage > pr) {
-									progress?.Report((pr = percentage) / 100.0);
-								}
+				string line;
+				double pr = 0.0;
+				using (var sr = process.StandardOutput) {
+					while ((line = await sr.ReadLineAsync()) != null) {
+						if (line.StartsWith("Playing ")) {
+							int stop = line.IndexOf('%');
+							if (stop > 8 && double.TryParse(line.Substring(8, stop - 8), out double percentage) && percentage > pr) {
+								progress?.Report((pr = percentage) / 100.0);
 							}
 						}
 					}
+				}
 
-					process.WaitForExit();
-					if (process.ExitCode != 0)
-						throw new AudioImporterException($"VGMPlay exited with status code {process.ExitCode}");
+				process.WaitForExit();
+				if (process.ExitCode != 0)
+					throw new AudioImporterException($"VGMPlay exited with status code {process.ExitCode}");
 
-					data = WaveConverter.FromFile(Path.Combine(tmpDir, "audio.wav"), true);
-					Directory.Delete(tmpDir, true);
-				} else if (Engine != null) {
-					data = await Engine.ReadFileAsync(filename, new Hints {
-						RenderingSampleRate = hints.RenderingSampleRate,
-						SampleCount = samples
-					}, progress);
-                } else {
-					throw new NotImplementedException();
-                }
+				PCM16Audio data = WaveConverter.FromFile(Path.Combine(tmpDir, "audio.wav"), true);
+				Directory.Delete(tmpDir, true);
+
 				if (loopSamples != 0) {
 					data.Looping = true;
 					data.LoopStart = (int)Math.Round((samples - loopSamples) * (hints.RenderingSampleRate / 44100.0));
